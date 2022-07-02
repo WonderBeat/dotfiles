0d094303761ba0d2512cfbb4814f04ae5630fb39
denisg <denisg.santiment.net>
2022-07-02 15:52:45 +0200

    home

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,20 @@ if status is-interactive
     set -U __done_notify_sound 1
     set -U __done_notification_urgency_level low
 
+    function vterm_printf;
+        if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+            # tell tmux to pass the escape sequences through
+            printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+        else if string match -q -- "screen*" "$TERM"
+            # GNU screen (screen, screen-256color, screen-256color-bce)
+            printf "\eP\e]%s\007\e\\" "$argv"
+        else
+            printf "\e]%s\e\\" "$argv"
+        end
+    end
 
     function vterm_prompt_end;
-    vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
+        vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
     end
     functions --copy fish_prompt vterm_old_fish_prompt
     function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
@@ -41,4 +52,18 @@ if status is-interactive
     end
 
     direnv hook fish | source
+
+alias mvn="mvn -T4"
+alias python=python3
+
+#config clone --bare git@github.com:WonderBeat/dotfiles.git ~/.myconf
+#config checkout
+alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
+
+# fish_add_path
+set -x VISUAL nvim
+set -x EDITOR "$VISUAL"
+set -x GOPATH $HOME/golang
+set -x GOROOT $BREW_PREFIX/opt/go/libexec
+set -x MANPATH "$BREW_PREFIX/opt/coreutils/libexec/gnuman:$MANPATH"
 end
